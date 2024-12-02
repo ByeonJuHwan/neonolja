6689ab176c008c8c2f51084859cae11c95e3ff35
byeonjuhwan <asdcz@naver.com>
2024-12-02 14:30:27 +0900

    최초커밋

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("kapt")
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.dev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // 스프링부트 기본설정
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(kotlin("test"))
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // mariDb
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ app/src/main/kotlin/com/dev/NeoNolJaApplication.kt
@@ -0,0 +1,11 @@
+package com.dev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeoNolJaApplication
+
+fun main(args: Array<String>) {
+    runApplication<NeoNolJaApplication>(*args)
+}
